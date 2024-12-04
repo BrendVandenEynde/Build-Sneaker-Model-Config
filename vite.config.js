034d629ad7becaa7fce1058fa2db17f84844a35c
Clueless <brendvandeneynde@hotmail.be>
2024-12-04 21:29:19 +0100

    fix #9: base path for deployment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/my-threejs-app/', // Customize the base path for deployment
+  base: '', // Customize the base path for deployment
   server: {
     open: true,
     port: 3000, // Specify a custom port for the dev server
