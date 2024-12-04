0044151bf2eb7b66a5a375cd2c6799d027f75f26
Clueless <brendvandeneynde@hotmail.be>
2024-12-04 21:47:04 +0100

    fix #11: remove antialias

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import { gsap } from '../node_modules/gsap/index.js';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-const renderer = new THREE.WebGLRenderer({ antialias: true });
+const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.getElementById('app').appendChild(renderer.domElement);
 
