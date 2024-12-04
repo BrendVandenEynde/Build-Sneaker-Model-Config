a9a026373031ebd1f4249e985a5f859bd70d599e
Clueless <brendvandeneynde@hotmail.be>
2024-12-04 22:14:55 +0100

    fix: remove high-preformance render setting

--- src/main.js
+++ src/main.js
@@ -7,7 +7,6 @@ const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 const renderer = new THREE.WebGLRenderer({
     antialias: true,
-    powerPreference: "high-performance"
 });
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.getElementById('app').appendChild(renderer.domElement);
